c533ee35e625d7f2e29a0be6a54d3676afc06fae
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-22 11:40:21 +0530

    Adding Voice function

--- public/script.js
+++ public/script.js
@@ -1,10 +1,29 @@
 const voiceSelect = document.querySelector('#voiceSelect')
 const playButton = document.querySelector('#playButton')
-const textInput = document.querySelector('#textInput')
+const textInput = document.querySelector('textarea')
+
+//Load available voices
+let voices = []
+function loadVoices(){
+    voices = speechSynthesis.getVoices()
+    voiceSelect.innerHTML = voices
+    .map(
+        (voice, index) =>
+             `<option value="${index}">${voice.name} (${voice.lang}) </option>`
+    )
+     .join('')
+}
+
+//Trigger loading voices when they become available
+speechSynthesis.onvoiceschanged = loadVoices;
+loadVoices();
+
 
 //Play TTS
 playButton.addEventListener('click', () => {
     const utterance = new SpeechSynthesisUtterance(textInput.value)
+    const selectedVoice = voices[voiceSelect.value];
+    if(selectedVoice) utterance.voice = selectedVoice;
     speechSynthesis.speak(utterance)
 })
 
